16b7218f9973b38d94d4134c99f0c3c96a0e198d
Sahil <leviack629@gmail.com>
2023-12-29 21:58:51 +0530

    patterns

--- javascript/patterns.js
+++ javascript/patterns.js
@@ -68,3 +68,21 @@ function printHollowSquarePattern(n) {
 }
 
 printHollowSquarePattern(5);
+console.log("");
+
+
+//fifth pattern
+function downwardStarPattern(n){
+  let string = ""
+  for(let i=0; i<=n; i++){
+    for(let j=0; j<n-i; j++){
+      string+="*"
+    }
+    string+='\n'
+  }
+  console.log(string);
+}
+downwardStarPattern(5)
+
+
+//sixth pattern
